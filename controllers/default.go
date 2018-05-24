e12ec59789279039080960b3dc1a3a0f4a38370c
yinhaozheng <77625961@qq.com>
2018-05-24 23:38:37 +0800

    fix

--- controllers/default.go
+++ controllers/default.go
@@ -8,8 +8,14 @@ type MainController struct {
 	beego.Controller
 }
 
-func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+func (this *MainController) Get() {
+	this.Layout = "layout.html"
+	this.LayoutSections = make(map[string]string)
+	this.LayoutSections["LayoutHeader"] = "baseLayout/header.html"
+	this.LayoutSections["NaviBar"] = "baseLayout/navibar.html"
+	this.LayoutSections["SearchWrapper"] = "baseLayout/searchwrapper.html"
+	this.LayoutSections["Footer"] = "baseLayout/footer.html"
+	this.LayoutSections["Script"] = "baseLayout/script.html"
+	this.TplName = "index/index.html"
 }
+
